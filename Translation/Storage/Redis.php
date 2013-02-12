f7fe0f450977bb43dec19e60f6f76247019a454d
oliviermadre <terenas@gmail.com>
2013-02-12 14:32:47 +0100

    Update Translation/Storage/Redis.php

--- Translation/Storage/Redis.php
+++ Translation/Storage/Redis.php
@@ -4,6 +4,8 @@ class Translation_Storage_Redis extends Translation_Storage_Abstract implements 
     protected $host = null;
     protected $port = null;
     
+    const REDIS_CACHE_KNOWN_KEYS = 'REDIS_CACHE_KNOWN_KEYS';
+    
     public function __construct($host = '127.0.0.1', $port = 6379) {
         $this->host = $host;
         $this->port = $port;
@@ -30,20 +32,25 @@ class Translation_Storage_Redis extends Translation_Storage_Abstract implements 
     
     public function get($key, $lang) {
         $this->init();
-        
+        $this->redisInstance->hGet($key, $lang);
         return false;
     }
 
     public function getAll() {
         $this->init();
-        
-        return false;
+        $aKeys = $this->redisInstance->hKeys(self::REDIS_CACHE_KNOWN_KEYS);
+        $result = array();
+        foreach($aKeys as $key) {
+            $result[$key] = $this->redisInstance->hGetAll($key);
+        }
+        return $result;
         
     }
 
     public function set($key, $lang, $value) {
         $this->init();
-        
+        $this->redisInstance->hSet($key, $lang, $value);
+        $this->redisInstance->hSet(self::REDIS_CACHE_KNOWN_KEYS, $key, 1);
         return false;
     }
 }
